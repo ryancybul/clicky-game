4572268460aabdbe9a8488b895e603531f2f74e3
Ryan Cybul <ryan.cybul@gmail.com>
2018-07-01 19:03:26 -0500

    Boiler plate

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+
+const App = () => (
+  <Router>
+    <div>
+      {/* <Nav /> Header */}
+      <Switch>
+      </Switch>
+      {/* <Nav /> Footer */}
+    </div>
+  </Router>
+);
+
+export default App;
