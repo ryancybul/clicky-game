579caff1eae16e18b0f487c9ffbda3c693d38ab1
Ryan Cybul <ryan.cybul@gmail.com>
2022-04-28 10:00:16 -0400

    Made header responsive

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -4,19 +4,14 @@ import "../Styles/Header.css";
 const Header = (props) => (
   <div className="headerWrapper">
     <div className="titleWrapper">
-      <ul>
-        <li>
-          <a href="/clicky-game/">{props.title}</a>
-        </li>
-
-        <li id="item-1">{props.correctIncorrect}</li>
-
-        <li id="item-2">Score: {props.score}</li>
-
-        <li id="item-3">Top Score: {props.topScore}</li>
-      </ul>
+      <h1 href="/clicky-game/">{props.title}</h1>
+      <div className="scoreBoard">
+        <p id="item-2">Score: {props.score}</p>
+        <p id="item-3">Top Score: {props.topScore}</p>
+      </div>
     </div>
     <div className="instructionsWrapper">
+      <p id="item-1">{props.correctIncorrect}</p>
       <h2>Try to click each character without clicky any single one twice</h2>
     </div>
   </div>
